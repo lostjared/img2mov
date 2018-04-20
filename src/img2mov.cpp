64dc316c83998d033cb1895ac58190cfe70d9bea
lostjared <jaredbruni@gmail.com>
2018-04-20 10:03:05 -0700

    removed whitespace

--- src/img2mov.cpp
+++ src/img2mov.cpp
@@ -86,15 +86,15 @@ cv::Mat img2mov::resizeKeepAspectRatio(const cv::Mat &input, const cv::Size &dst
     cv::Mat output;
     double h1 = dstSize.width * (input.rows/(double)input.cols);
     double w2 = dstSize.height * (input.cols/(double)input.rows);
-    if( h1 <= dstSize.height) {
+    if(h1 <= dstSize.height) {
         cv::resize(input, output, cv::Size(dstSize.width, h1));
     } else {
         cv::resize(input, output, cv::Size(w2, dstSize.height));
     }
-    int top = (dstSize.height-output.rows) / 2;
-    int down = (dstSize.height-output.rows+1) / 2;
-    int left = (dstSize.width - output.cols) / 2;
-    int right = (dstSize.width - output.cols+1) / 2;
+    int top = (dstSize.height-output.rows)/2;
+    int down = (dstSize.height-output.rows+1)/2;
+    int left = (dstSize.width - output.cols)/2;
+    int right = (dstSize.width - output.cols+1)/2;
     cv::copyMakeBorder(output, output, top, down, left, right, cv::BORDER_CONSTANT, bgcolor);
     return output;
 }
