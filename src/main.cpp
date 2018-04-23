e185e261d7a10dda5b8b27918006fa783999c801
lostjared <jaredbruni@gmail.com>
2018-04-23 08:05:39 -0700

    better output of program name

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,8 @@ void control_Handler(int sig) {
     std::cout << "\nimg2mov: Signal caught stopping...\n";
 }
 
-void printInfo() {
-    std::cout << "To use: img2mov\n\t-v print version info\n\t-i input directory\n\t-r \"match with regular expression\"\n\t-t input file list in text file\n\t-o output video file mov\n\t-w frame width\n\t-h frame height\n\t-f frames per second\n\t-s stretch image (if not set will resize to keep aspect ratio)\n\t-n do not sort list of files..\n";
+void printInfo(const char *program) {
+    std::cout << "To use: " << program << "\n\t-v print version info\n\t-i input directory\n\t-r \"match with regular expression\"\n\t-t input file list in text file\n\t-o output video file mov\n\t-w frame width\n\t-h frame height\n\t-f frames per second\n\t-s stretch image (if not set will resize to keep aspect ratio)\n\t-n do not sort list of files..\n";
 }
 
 int main(int argc, char **argv) {
@@ -57,16 +57,16 @@ int main(int argc, char **argv) {
                     no_sort = true;
                     break;
                 default:
-                    std::cout << "img2mov: invalid option...\n";
-                    printInfo();
+                    std::cout << argv[0] << ": invalid option...\n";
+                    printInfo(argv[0]);
                     exit(0);
                     break;
             }
         }
         try {
             if(width == 0  || height == 0 || fps == 0 || (dir_name.length() == 0 && text_file.length() == 0) || file_name.length() == 0) {
-                std::cerr << "img2mov: Requires input/output flags..\n";
-                printInfo();
+                std::cerr << argv[0] << ": Requires input/output flags..\n";
+                printInfo(argv[0]);
                 exit(EXIT_FAILURE);
             }
             struct sigaction sa;
@@ -74,12 +74,12 @@ int main(int argc, char **argv) {
             sa.sa_flags = 0;
             sa.sa_handler = control_Handler;
             if(sigaction(SIGINT, &sa, 0) == -1) {
-                std::cerr << "img2mov: Error on sigaction:\n";
+                std::cerr << argv[0] << ": Error on sigaction:\n";
                 exit(EXIT_FAILURE);
             }
             if(img2mov::toLower(file_name).find("mov") == std::string::npos)
                 file_name += ".mov";
-            std::cout << "img2mov: Press Ctrl+C to stop processing...\n";
+            std::cout << argv[0] << ": Press Ctrl+C to stop processing...\n";
             program.reset(new img2mov(dir_name, file_name,fps,width,height,stretch, no_sort));
             if(text_file.length() > 0)
                 program->setList(text_file);
@@ -88,12 +88,12 @@ int main(int argc, char **argv) {
             
             program->run();
         } catch(std::exception &e) {
-            std::cerr << "Exception: " << e.what() << "\n";
+            std::cerr << argv[0] << ": Exception: " << e.what() << "\n";
             exit(EXIT_FAILURE);
         }
     } else {
-        std::cerr << "img2mov: Requires input/output flags..\n";
-        printInfo();
+        std::cerr << argv[0] << ": Requires input/output flags..\n";
+        printInfo(argv[0]);
         exit(EXIT_FAILURE);
     }
     return 0;
